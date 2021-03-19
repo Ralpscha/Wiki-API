c706bcb5751df432ebcdccb4efadbf28183bf047
Ralph van Schaik <ralph.van.schaik@me.com>
2021-03-19 14:56:55 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended = true}));
+app.use(express.static('public'));
+
+mongoose.connect("mongodb://localhost: 27017/wikiDB", {
+  useNewUrlParser: true
+});
+
+const articleSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+
+app.listen(3000, function (){
+  console.log("Server gestart op poort 3000");
+});
