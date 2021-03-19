fca2f7f945737186e310fc7d0695ffdae517da44
Ralph van Schaik <ralph.van.schaik@me.com>
2021-03-19 15:38:26 +0100

    Toon alle artikelen

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended = true}));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
 
 mongoose.connect("mongodb://localhost: 27017/wikiDB", {
@@ -20,6 +20,15 @@ const articleSchema = {
 };
 
 const Article = mongoose.model("Article", articleSchema);
+
+
+app.get('/articles', function (req, res) {
+  Article.find( {}, function (err, foundArticles) {
+    console.log(foundArticles);
+    res.render('articles', {foundArticles: foundArticles});
+  })
+})
+
 
 
 app.listen(3000, function (){
