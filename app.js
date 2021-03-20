a3993b76c76aa2c2a4bb348e40a4733b529c52c9
Ralph van Schaik <ralph.van.schaik@me.com>
2021-03-20 15:57:53 +0100

    368 delete all articles

--- app.js
+++ app.js
@@ -50,6 +50,16 @@ app.post('/articles', function (req,res){
   });
 });
 
+app.delete('/articles', function (req, res){
+  Article.deleteMany( function (err) {
+    if(!err) {
+      res.send('Succesfully deleted all');
+    } else {
+      res.send(err);
+    }
+  });
+})
+
 app.listen(3000, function (){
   console.log("Server gestart op poort 3000");
 });
