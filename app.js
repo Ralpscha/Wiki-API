6b1cb2dd929df19ed607e6bc2fe484ab1a887c59
Ralph van Schaik <ralph.van.schaik@me.com>
2021-03-20 16:14:53 +0100

    369 use the app.route method

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 mongoose.connect("mongodb://localhost: 27017/wikiDB", {
@@ -21,45 +21,84 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+app.route('/articles')
+    .get(function (req, res) {
+      Article.find({}, function (err, foundArticles) {
+        // console.log(foundArticles);
+        // res.render('articles', {foundArticles: foundArticles});
+        if (!err) {
+          res.send(foundArticles);
+        } else {
+          res.send(err);
+        }
+      });
+    })
 
-app.get('/articles', function (req, res) {
-  Article.find( {}, function (err, foundArticles) {
-    // console.log(foundArticles);
-    // res.render('articles', {foundArticles: foundArticles});
-    if(!err){
-      res.send(foundArticles);
-    } else{
-      res.send(err);
-    }
-  });
-});
+    .post(function (req, res) {
 
-app.post('/articles', function (req,res){
+      const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+      });
+
+      newArticle.save(function (err) {
+        if (!err) {
+          res.send("Succesfully added a new article");
+        } else {
+          res.send(err);
+        }
+      });
+    })
+
+    .delete(function (req, res) {
+      Article.deleteMany(function (err) {
+        if (!err) {
+          res.send('Succesfully deleted all');
+        } else {
+          res.send(err);
+        }
+      });
+    });
+
+
+// app.get('/articles', function (req, res) {
+//   Article.find( {}, function (err, foundArticles) {
+//     // console.log(foundArticles);
+//     // res.render('articles', {foundArticles: foundArticles});
+//     if(!err){
+//       res.send(foundArticles);
+//     } else{
+//       res.send(err);
+//     }
+//   });
+// });
+//
+app.post('/articles', function (req, res) {
 
   const newArticle = new Article({
     title: req.body.title,
     content: req.body.content
   });
 
-  newArticle.save(function (err){
-    if(!err) {
+  newArticle.save(function (err) {
+    if (!err) {
       res.send("Succesfully added a new article");
     } else {
       res.send(err);
     }
   });
 });
+//
+// app.delete('/articles', function (req, res){
+//   Article.deleteMany( function (err) {
+//     if(!err) {
+//       res.send('Succesfully deleted all');
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// })
 
-app.delete('/articles', function (req, res){
-  Article.deleteMany( function (err) {
-    if(!err) {
-      res.send('Succesfully deleted all');
-    } else {
-      res.send(err);
-    }
-  });
-})
-
-app.listen(3000, function (){
+app.listen(3000, function () {
   console.log("Server gestart op poort 3000");
 });
