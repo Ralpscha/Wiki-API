b234e469766777a8b591f039acb406bca1d154c9
Ralph van Schaik <ralph.van.schaik@me.com>
2021-03-21 11:56:54 +0100

    373 delete a specific article

--- app.js
+++ app.js
@@ -99,6 +99,16 @@ app.route('/articles/:articleTitle')
             }
           }
       );
+    })
+
+    .delete(function (req, res){
+      Article.deleteOne({title: req.params.articleTitle}, function (err){
+        if(!err){
+          res.send("Article succesfully deleted");
+        } else{
+          res.send(err);
+        }
+      })
     });
 
 app.listen(3000, function () {
